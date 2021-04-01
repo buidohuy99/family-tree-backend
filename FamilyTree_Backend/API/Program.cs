b8e2f409a01683c95504c8d68285bdca9c1291cc
buidohuy99 <sunnyhill93@gmail.com>
2021-04-01 08:08:32 +0700

    initial docker setup (doesnt work)

--- FamilyTree_Backend/API/Program.cs
+++ FamilyTree_Backend/API/Program.cs
@@ -16,9 +16,11 @@ namespace FamilyTreeBackend.Presentation.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:3000/");
                     webBuilder.ConfigureLogging((logging) => {
                         logging.ClearProviders();
                         logging.AddSerilog();
+                        logging.AddConsole();
                     });
                     webBuilder.UseStartup<App>();
                 });
