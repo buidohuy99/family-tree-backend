6b9b4c028e3d972b263e881807a04c3202b4b1ec
johnnyshi1999 <johnnyshi99@gmail.com>
2021-04-22 22:01:05 +0700

    add tree editors & owner

---
+++ FamilyTree_Backend/Domain/Entities/MetaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTreeBackend.Core.Domain.Entities
+{
+    public interface MetaData
+    {
+        public DateTime? DateCreated { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
