8404ca6975ebf5497a07af267f621e72df40f614
johnnyshi1999 <johnnyshi99@gmail.com>
2021-06-13 18:36:24 +0700

    change data view to show json data

--- FamilyTree_Backend/ThirdPartyServices/MapperProfiles/RequestResponseLogProfile.cs
+++ FamilyTree_Backend/ThirdPartyServices/MapperProfiles/RequestResponseLogProfile.cs
@@ -2,10 +2,12 @@
 using FamilyTreeBackend.Core.Application.Operation.Models;
 using FamilyTreeBackend.Core.Domain.Entities;
 using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FamilyTreeBackend.Infrastructure.Service.ThirdPartyServices.MapperProfiles
@@ -20,7 +22,18 @@ namespace FamilyTreeBackend.Infrastructure.Service.ThirdPartyServices.MapperProf
 
             CreateMap<RequestResponseDataModel, RequestResponseLogDetailsModel>()
                 .ForMember(dest => dest.StatusCode, opt =>
-                    opt.MapFrom(src => $"{src.StatusCode}-{ReasonPhrases.GetReasonPhrase(src.StatusCode)}"));
+                    opt.MapFrom(src => $"{src.StatusCode}-{ReasonPhrases.GetReasonPhrase(src.StatusCode)}"))
+                .ForMember(dest => dest.RequestBody, opt =>
+                    opt.MapFrom(src => PretifyJsonString(src.RequestBody)))
+                .ForMember(dest => dest.ResponseBody, opt =>
+                    opt.MapFrom(src => PretifyJsonString(src.ResponseBody)));
+        }
+
+        private static string PretifyJsonString(string json)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            string result = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+            return result;
         }
     }
 }
