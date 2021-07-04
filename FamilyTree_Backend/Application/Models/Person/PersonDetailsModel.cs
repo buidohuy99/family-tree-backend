2b830c9a6d8c3243cd7202611e9630ce869a344c
johnnyshi1999 <johnnyshi99@gmail.com>
2021-07-04 16:27:23 +0700

    add user icon to person info

--- FamilyTree_Backend/Application/Models/Person/PersonDetailsModel.cs
+++ FamilyTree_Backend/Application/Models/Person/PersonDetailsModel.cs
@@ -25,6 +25,7 @@ namespace FamilyTreeBackend.Core.Application.Models.Person
         public string Occupation { get; set; }
         public string Note { get; set; }
         public string UserId { get; set; }
+        public UserIconDTO ConnectedUser { get; set; }
         public string ImageUrl { get; set; }
     }
 }
